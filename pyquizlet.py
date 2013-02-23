5dfb61a93d4eb4e7b2c707b53f5633f1b1408cb5
Tobias Domhan <tdomhan@gmail.com>
2013-02-23 13:39:30 -0500

    authentication

--- pyquizlet.py
+++ pyquizlet.py
@@ -1,19 +1,66 @@
 import httplib
+import httplib2
 import json
 import urllib
+import random
+import string
+import base64
+
+#author: Tobias Domhan
 
 
 class Quizlet():
     def __init__(self, qid):
         self.qid = qid
         self.base_url = "https://api.quizlet.com/2.0/"
-    
+        self.authorized = False
+        self.access_token = None
+
+    #generate an authentication url 
+    #redirect the user to this url
+    def generate_auth_url(self, scopes):
+        #TODO: check if scope is a list of strings
+        #TODO: url redirect parameter
+        auth_url = 'https://quizlet.com/authorize/'
+        state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))
+        params = {'scope': " ".join(scopes),
+                  'client_id': self.qid,
+                  'response_type': 'code',
+                  'state': state }
+        request_string = auth_url + '?' + urllib.urlencode(params)
+
+        return (request_string,state)
+
+    def request_token(self, code, redirect_uri, secret):
+        self.authorized = False
+        auth_url = 'https://api.quizlet.com/oauth/token'
+        params = {'grant_type': 'authorization_code',
+                  'code': code,
+                  'redirect_uri': redirect_uri}
+        auth = base64.encodestring( self.qid + ':' + secret)
+        headers = {'Content-type': 'application/x-www-form-urlencoded',
+                   'Authorization' : 'Basic ' + auth}
+
+        h = httplib2.Http()
+        #h.add_credentials(self.qid, secret)
+
+        print urllib.urlencode(params)
+
+        response, content = h.request(auth_url, "POST", headers=headers, body=urllib.urlencode(params))
+
+        if response['status'] != '200':
+            raise Exception("request not successful(return code: %s): %s" % (response['status'], content))
+
+        response_data = json.loads(content)
+        self.access_token = response_data
+        self.authorized = True
+
     def make_request(self, apistring, params={}):
         base_url = self.base_url + apistring
         params['client_id'] = self.qid
         connection = httplib.HTTPSConnection('quizlet.com')
         request_string = base_url + '?' + urllib.urlencode(params)
-        print request_string
+
         connection.request('GET', request_string)
         response = connection.getresponse()
 
@@ -36,13 +83,9 @@ class Quizlet():
             results.append(r)
         return results
 
-    def get_set(self, setid, paged=True):
+    def get_set(self, setid):
         s = "sets/%s" % setid
-        if not paged:
-            return self.make_request(s)
-        else:
-            return self.make_paged_request(s)
-
+        return self.make_request(s)
 
 
     def search_sets(self, sstring, paged=True):
